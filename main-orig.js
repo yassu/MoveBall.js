0da3c16e6af94e78c2441416fcb945dccfe8f09f
yassu <mathyassu@gmail.com>
2016-01-21 05:51:50 +0900

    #05: アニメーションを作ってみよう

--- main-orig.js
+++ main-orig.js
@@ -5,11 +5,12 @@ window.onload = function(){
   var core = new Core(CORE_SIZE[0], CORE_SIZE[1]);
   var MAX_MOVE = 10;
   var IMAGE_SIZE = [20, 20];
-  core.preload('ball.png');
+  core.preload('balls.png');
+  core.fps = 15;
 
   core.onload = function(){
     var ball = new Sprite(IMAGE_SIZE[0], IMAGE_SIZE[1]);
-    ball.image = core.assets['ball.png'];
+    ball.image = core.assets['balls.png'];
     ball.x = CORE_SIZE[0] / 2;
     ball.y = CORE_SIZE[1] / 2;
     core.rootScene.addChild(ball);
@@ -18,15 +19,20 @@ window.onload = function(){
     ball.theta = Math.PI * Math.random();
     ball.move_x = ball.move_r * Math.cos(ball.theta);
     ball.move_y = ball.move_r * Math.sin(ball.theta);
+    hit_cnt = 0;
     ball.addEventListener('enterframe', function(){
       this.x += this.move_x;
       this.y += this.move_y;
       if((this.x > CORE_SIZE[0] - IMAGE_SIZE[0] && this.move_x > 0) ||
          (this.x < 0 && this.move_x < 0)) {
         this.move_x *= -1;
+        hit_cnt += 1;
+        this.frame = (this.frame + 1) % 3;
       } else if ((this.y > CORE_SIZE[1] - IMAGE_SIZE[1] && this.move_y > 0) ||
         (this.y < 0 && this.move_y < 0)) {
         this.move_y *= -1;
+        hit_cnt += 1;
+        this.frame = (this.frame + 1) % 3;
       }
       console.log(this.move_x);
     });
